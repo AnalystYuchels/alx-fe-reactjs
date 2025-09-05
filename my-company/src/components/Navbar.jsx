dd0910edefdadda235f901018206018dfb0badd0
AnalystYuchels <uche4xploit@gmail.com>
2025-09-05 13:02:01 +0100

    Updated the navbar.jsx file

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,38 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#333" }}>
-      <Link to="/" style={{ margin: "0 10px", color: "white", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px", color: "white", textDecoration: "none" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px", color: "white", textDecoration: "none" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px", color: "white", textDecoration: "none" }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "1rem",
+        backgroundColor: "#333",
+        color: "white",
+      }}
+    >
+      <h2>My Website</h2>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+        }}
+      >
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+          About
+        </Link>
+        <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
+          Services
+        </Link>
+        <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
-
-export default Navbar;
